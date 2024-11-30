5848c4a7cec94c1196310d96aadbf34e3b2dd464
Nhu Khanh <21522204@gm.uit.edu.vn>
2024-12-01 04:26:18 +0700

    brrrrrrrrrr

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -5,7 +5,7 @@ import { dirname, join } from "path";
 
 // import type { StorybookConfig } from "@storybook/react-vite";
 
-import { join, dirname } from "path";
+// import { join, dirname } from "path";
 
 // /**
 //  * This function is used to resolve the absolute path of a package.
