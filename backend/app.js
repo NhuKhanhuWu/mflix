b5d3f3acb137a0eb4adc706070d74a26a5e18490
Nhu Khanh <21522204@gm.uit.edu.vn>
2025-03-24 01:28:13 +0700

    update

--- backend/app.js
+++ backend/app.js
@@ -2,16 +2,26 @@
 
 const express = require("express");
 const cors = require("cors");
+const AppError = require("./api/utils/appError");
+const globalErrHandler = require("./api/controllers/errorController");
 
 const movieRouter = require("./api/routes/movieRouter");
 const commentRouter = require("./api/routes/commentRouter");
 
 const app = express();
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
 
 // ROUTER
 app.use("/api/v1/movies", movieRouter);
 app.use("/api/v1/movies/:movie_id/comments", commentRouter);
+app.use("/api/v1/genres/", movieRouter);
+
+// ERROR
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 400));
+});
+
+app.use(globalErrHandler);
 
 module.exports = app;
