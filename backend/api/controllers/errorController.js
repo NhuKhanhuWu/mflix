6cbf6045b76cf6cf97c5d8f306a0316eac8bac10
Nhu Khanh <21522204@gm.uit.edu.vn>
2025-08-05 23:05:28 +0700

    nothing changed

--- backend/api/controllers/errorController.js
+++ backend/api/controllers/errorController.js
@@ -33,6 +33,7 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
+  return sendErrorDev(err, res);
   if (process.env.NODE_ENV === "development") {
     return sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === "production") {
